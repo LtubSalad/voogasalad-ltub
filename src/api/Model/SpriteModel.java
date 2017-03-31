e442fb93d1b721dac80e06687a28a77e368eaa27
Tahia Emran <tahia.emran@duke.edu>
2017-03-30 23:03:54 -0400

    commented interfaces

--- src/api/Model/SpriteModel.java
+++ src/api/Model/SpriteModel.java
@@ -3,5 +3,8 @@ package api.Model;
 import java.util.Collection;
 
 public interface SpriteModel {
+	/**
+	 * @return a read-only collection of the sprites in the model
+	 */
 	public Collection<Object> getReadOnlySprites(); 
 }
