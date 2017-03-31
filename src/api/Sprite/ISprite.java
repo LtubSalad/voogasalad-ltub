9d40f375744e903c1540c7b5196ec8ffc2236ea7
Matthew Tribby <mrt28@duke.edu>
2017-03-30 23:11:05 -0400

    Add comments to sprite and tile

--- src/api/Sprite/ISprite.java
+++ src/api/Sprite/ISprite.java
@@ -1,7 +1,18 @@
 package api.Sprite;
 
+/**
+ * Public methods for the Sprite which are needed to ensure their proper behavior
+ * and dynamic changeability in the game play.
+ * @author Matthew Tribby
+ */
 public interface ISprite {
 
+	/**
+	 * Sets the attribute of the sprite. 
+	 * @param attributeType Name of the attribute type which will allow for
+	 * 						changing the behavior of the sprite through changing
+	 * 						the attribute of the sprite
+	 */
 	public void setAttribute(String attributeType);
 
 }
