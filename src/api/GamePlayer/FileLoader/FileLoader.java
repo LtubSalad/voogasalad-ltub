66eece4e98444e87cd2b4038126bbc740056210a
Yilin Gao <yilin.gao@duke.edu>
2017-03-30 23:08:11 -0400

    add comments to api code

--- src/api/GamePlayer/FileLoader/FileLoader.java
+++ src/api/GamePlayer/FileLoader/FileLoader.java
@@ -1,5 +1,14 @@
 package api.GamePlayer.FileLoader;
 
+/**
+ * the interface to load game files in the game player
+ * @author Yilin Gao
+ *
+ */
 public interface FileLoader {
+	
+	/**
+	 * the method to ask the user to choose his desired game file
+	 */
 	public void loadGameFile();
 }
