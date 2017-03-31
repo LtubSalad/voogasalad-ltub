3a2243d8f88f07e7b2a2d401b55ea4e22c756a47
Matthew Tribby <mrt28@duke.edu>
2017-03-31 00:22:17 -0400

    Example case, another one:

--- src/examples/EventBus.java
+++ src/examples/EventBus.java
@@ -1,6 +1,11 @@
 package examples;
 
-public class EventBus {
+import api.EventBus.IEventBus;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+
+public class EventBus implements IEventBus {
 	String name; 
 	public EventBus(){
 		name = "tester";
@@ -8,4 +13,20 @@ public class EventBus {
 	public void emit(spriteAttackedEvent ev){
 		; 
 	}
+	@Override
+	public <T extends Event> void on(EventType<T> et, EventHandler<? super T> eh) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public <T extends Event> void off(EventType<T> et, EventHandler<? super T> eh) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void emit(Event ev) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
