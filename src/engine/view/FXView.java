4e619cbb40499de907b4ac1b31d27667cbe86edf
Yilin Gao <yilin.gao@duke.edu>
2017-04-03 23:27:08 -0400

    update to pull

--- src/engine/view/FXView.java
+++ src/engine/view/FXView.java
@@ -1,25 +1,32 @@
 package engine.view;
 
 import engine.sprite.Sprite;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 
 public class FXView implements View {
 
+	Group root = new Group();
+	Scene scene = new Scene(root);
+	
+	public FXView() {
+		
+	}
+	
 	@Override
 	public void addSprite(Sprite sprite) {
-		
+		// TODO
+		root.getChildren().add(null);
 	}
 
 	@Override
 	public void removeSprite(Sprite sprite) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
 
 	@Override
 	public Scene getScene() {
-		// TODO Auto-generated method stub
-		return null;
+		return scene;
 	}
 
 }
