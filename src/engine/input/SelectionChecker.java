80d1f7e85b94377bf81f1c46a712428ae0afd3ed
Yilin Gao <yilin.gao@duke.edu>
2017-04-04 02:47:05 -0400

    update SelectionChecker

--- src/engine/input/SelectionChecker.java
+++ src/engine/input/SelectionChecker.java
@@ -1,6 +1,12 @@
 package engine.input;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import engine.model.Model;
+import engine.sprite.Sprite;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class SelectionChecker {
 	
@@ -9,11 +15,27 @@ public class SelectionChecker {
 		
 	}
 	
-	public boolean checkSelection(Model model, double x, double y) {
-		return true;
+	public Sprite checkSelection(Model model, double x, double y) {
+		for (Sprite sprite: model.getSprites()) {
+			Image spriteImage = new Image(sprite.getImage().getInputStream());
+			ImageView spriteImageView = new ImageView(spriteImage);
+			List<Double> xList = new ArrayList<>(); // ImageView rectangle nodes are added in the clock-wise order
+			xList.add(spriteImageView.getX());
+			xList.add(spriteImageView.getX() + spriteImageView.getFitWidth());
+			xList.add(spriteImageView.getX() + spriteImageView.getFitWidth());
+			xList.add(spriteImageView.getX());
+			List<Double> yList = new ArrayList<>();
+			yList.add(spriteImageView.getY());
+			yList.add(spriteImageView.getY());
+			yList.add(spriteImageView.getY() + spriteImageView.getFitHeight());
+			yList.add(spriteImageView.getY() + spriteImageView.getFitHeight());
+			if (rayCasting(4, xList, yList, x, y)) {
+				return sprite;
+			}
+		}
 	}
 
-	private void rayCasting() {
-		
+	private boolean rayCasting(int nVert, List<Double> xList, List<Double> yList, double xClicked, double yClicked ) {
+		return true;
 	}
 }
