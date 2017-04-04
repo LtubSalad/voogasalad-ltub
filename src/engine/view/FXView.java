d2d1edc382b063cc9bddf0b1cdfea933cf864e1c
Yilin Gao <yilin.gao@duke.edu>
2017-04-03 23:39:57 -0400

    update FXView

--- src/engine/view/FXView.java
+++ src/engine/view/FXView.java
@@ -3,20 +3,31 @@ package engine.view;
 import engine.sprite.Sprite;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class FXView implements View {
 
-	Group root = new Group();
-	Scene scene = new Scene(root);
+	private Group root;
+	private Scene scene;
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 400;
+	public static final Paint BACKGROUND = Color.BISQUE;
 	
 	public FXView() {
-		
+		root = new Group();
+		scene = new Scene(root, WIDTH, HEIGHT, BACKGROUND);
 	}
 	
 	@Override
 	public void addSprite(Sprite sprite) {
 		// TODO
-		root.getChildren().add(null);
+		engine.sprite.Image spriteImage = sprite.getImage();
+		String spriteImageFileName = spriteImage.getFileName();
+		javafx.scene.image.Image realImage = new javafx.scene.image.Image(getClass().getClassLoader().getResourceAsStream(spriteImageFileName));
+		ImageView realImageView = new ImageView(realImage);
+		root.getChildren().add(realImageView);
 	}
 
 	@Override
