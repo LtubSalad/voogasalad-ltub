7561e70c33c356a72586f36e457a084eba34eb87
Yilin Gao <yilin.gao@duke.edu>
2017-04-03 23:07:51 -0400

    update sound on ubuntu

--- src/engine/sound/FXSoundManager.java
+++ src/engine/sound/FXSoundManager.java
@@ -1,9 +1,10 @@
 package engine.sound;
 
 import java.io.File;
-
+import static commons.RunningMode.DEV_MODE;
 import bus.EventBus;
 import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
 import javafx.scene.media.MediaPlayer;
 
 public class FXSoundManager implements SoundManager {
@@ -24,8 +25,14 @@ public class FXSoundManager implements SoundManager {
 	@Override
 	public void play(String soundFileName) {
 		Media sound = new Media(new File(soundFileName).toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(sound);
-		mediaPlayer.play();
+		try {
+			MediaPlayer mediaPlayer = new MediaPlayer(sound);
+			mediaPlayer.play();
+		}catch (MediaException e) {
+			if (DEV_MODE) {
+				e.printStackTrace();
+			}
+		}		
 	}
 	
 
