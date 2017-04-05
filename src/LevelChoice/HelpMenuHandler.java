b950c1aef503af3f9cdcbb75586b878027495234
Zhiyong Zhao <zhiyong.zhao@duke.edu>
2017-04-05 00:04:59 -0400

    menu bar

---
+++ src/LevelChoice/HelpMenuHandler.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package LevelChoice;
+
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+
+/**
+ * @author Zhiyong
+ *
+ */
+public class HelpMenuHandler implements MenuItemHandler{
+	private MenuItem viewHelp;
+	public HelpMenuHandler(String source){
+		viewHelp = new MenuItem(source);
+		viewHelp.setAccelerator(new KeyCodeCombination(KeyCode.H, KeyCombination.SHORTCUT_DOWN, KeyCombination.SHIFT_DOWN));
+		viewHelp.setOnAction(e -> handle());
+	}
+
+	@Override
+	public void handle() {
+		
+		
+	}
+
+	@Override
+	public MenuItem getMenuItem() {
+
+		return viewHelp;
+	}
+	
+}
---
+++ src/LevelChoice/MenuItemHandler.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package LevelChoice;
+
+import javafx.scene.control.MenuItem;
+
+/**
+ * @author Zhiyong
+ *
+ */
+public interface MenuItemHandler {
+	
+	public void handle();
+	
+	public MenuItem getMenuItem();
+
+}
