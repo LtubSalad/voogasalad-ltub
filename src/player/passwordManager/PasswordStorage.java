d0edff57db7a6c455910bd9db06094148b1c2d32
Zhiyong Zhao <zhiyong.zhao@duke.edu>
2017-04-11 20:36:32 -0400

    password fixed

--- src/player/passwordManager/PasswordStorage.java
+++ src/player/passwordManager/PasswordStorage.java
@@ -4,9 +4,12 @@
 package player.passwordManager;
 
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
  
@@ -15,6 +18,8 @@ import java.util.Properties;
  *
  */
 public class PasswordStorage {
+	private String userName;
+	private String password;
 
  
     public static void main(String a[]) throws IOException{
@@ -32,4 +37,43 @@ public class PasswordStorage {
         }
          
     }
+    
+    public void loadParams() {
+        Properties props = new Properties();
+        InputStream is = null;
+     
+        // First try loading from the current directory
+        try {
+            File f = new File("server.properties");
+            is = new FileInputStream( f );
+        }
+        catch ( Exception e ) { is = null; }
+     
+        try {
+            if ( is == null ) {
+                // Try loading from classpath
+                is = getClass().getResourceAsStream("server.properties");
+            }
+     
+            // Try loading properties from the file (if found)
+            props.load( is );
+        }
+        catch ( Exception e ) { }
+    }
+    
+    //TODO:need to store the user registration
+    public void saveParamChanges(String inputUser, String inputPassword) {
+        try {
+            Properties props = new Properties();
+            props.setProperty("ServerAddress", userName);
+            props.setProperty("ServerPort", password);
+            File f = new File("myProp.properties");
+            OutputStream out = new FileOutputStream( f );
+            props.store(out, "This is an optional header comment string");
+        }
+        catch (Exception e ) {
+            e.printStackTrace();
+        }
+    }
+    
 }
