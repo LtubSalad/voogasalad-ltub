c5b981ab2958b8ba0aa19ca4eb3444e030325b11
Yilin Gao <yilin.gao@duke.edu>
2017-04-12 23:14:26 -0400

    add comments

--- src/engine/sprite/range/InRangeEvent.java
+++ src/engine/sprite/range/InRangeEvent.java
@@ -15,17 +15,30 @@ public class InRangeEvent extends BusEvent {
 	
 	private Sprite detector;
 	private Sprite detectee;
-	
+	/**
+	 * Constructor for {@code InRangeEvent}.
+	 * @param busEventType the {@code BusEventType} for the {@code InRangeEvent}.
+	 * @param detector the {@code Sprite} which detects some other {@code Sprite} is in its detection range.
+	 * @param detectee the {@code Sprite} which gets detected by some other {@code Sprite}.
+	 */
 	public InRangeEvent(BusEventType<? extends BusEvent> busEventType, Sprite detector, Sprite detectee) {
 		super(busEventType);
 		this.detector = detector;
 		this.detectee = detectee;
 	}
 
+	/**
+	 * Get the {@code Sprite} which detects some other {@code Sprite} is in its detection range.
+	 * @return Sprite
+	 */
 	public Sprite getDetector() {
 		return detector;
 	}
 	
+	/**
+	 * Get the {@code Sprite} which gets detected by some other {@code Sprite}.
+	 * @return Sprite
+	 */
 	public Sprite getDetectee() {
 		return detectee;
 	}
