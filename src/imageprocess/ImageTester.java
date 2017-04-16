2c0c8aa98feaaa9d32e15dbeac62e64af6089db3
Zhiyong Zhao <zhiyong.zhao@duke.edu>
2017-04-15 21:13:55 -0400

    fix noise for image process

--- src/imageprocess/ImageTester.java
+++ src/imageprocess/ImageTester.java
@@ -18,40 +18,40 @@ import javafx.stage.Stage;
  *
  *Test the convex_hull or Hull methods by providing an image
  *
- *Usage: please change the name of the image directory: "resources/bahamut_left.png"
+ *Usage: please change the name of the image directory: "resources/home.png"
  */
 
 public class ImageTester extends Application{
-	public static final String IMAGE_DIRECTORY = "resources/bahamut_left.png";
+	public static final String IMAGE_DIRECTORY = "resources/home.png";
 	public static void main(String[] args){
 		launch(args);
-		}
-		
+	}
 
-	
+
+
 	public void start(Stage primaryStage){
-		 primaryStage = new Stage();
-			primaryStage.setTitle("Test Image");
-			Group root = new Group();
-			Scene scene = new Scene(root, 800, 800);
-			
-			Image image = new Image(IMAGE_DIRECTORY);
-			
-			Rectangle r = new Rectangle(image.getWidth(),image.getHeight());
-			r.setFill(new ImagePattern(image));
+		primaryStage = new Stage();
+		primaryStage.setTitle("Test Image");
+		Group root = new Group();
+		Scene scene = new Scene(root, 800, 800);
 
-           ImageTransformation it = new ImageTransformation();
+		Image image = new Image(IMAGE_DIRECTORY);
 
-             root.getChildren().addAll(r);
-             
-             ImageToPolygon im = new ImageToPolygon(image);
-             Polygon pol = im.getPolygon();
-             pol.setLayoutX(100);
-             pol.setLayoutY(100);
-             pol.setFill(Color.RED);
-         root.getChildren().add(pol);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+		Rectangle r = new Rectangle(image.getWidth(),image.getHeight());
+		r.setFill(new ImagePattern(image));
+
+		//ImageTransformation it = new ImageTransformation();
+
+		root.getChildren().addAll(r);
+
+		ImageToPolygon im = new ImageToPolygon(image);
+		Polygon pol = im.getPolygon();
+		pol.setLayoutX(200);
+		pol.setLayoutY(200);
+		pol.setFill(Color.RED);
+		root.getChildren().add(pol);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 }
