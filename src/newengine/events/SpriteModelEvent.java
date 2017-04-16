d2bc0592ae31172efd2202967bc8b63ee4edfa0a
Keping <kw238@duke.edu>
2017-04-16 19:23:15 -0400

    remove comments

--- src/newengine/events/SpriteModelEvent.java
+++ src/newengine/events/SpriteModelEvent.java
@@ -9,24 +9,15 @@ import newengine.sprite.Sprite;
 public class SpriteModelEvent extends BusEvent {
 
 	public static final BusEventType<SpriteModelEvent> ADD = new BusEventType<>();
-//	public static final BusEventType<SpriteModelEvent> ADD_LIST = new BusEventType<>();
 	public static final BusEventType<SpriteModelEvent> REMOVE = new BusEventType<>();
 	
-//	private Sprite sprite; // TODO better distinguish single and list
 	private List<Sprite> sprites;
 	
-//	public SpriteModelEvent(BusEventType<? extends BusEvent> busEventType, Sprite sprite) {
-//		super(busEventType);
-//		this.sprite = sprite;
-//	}
 	public SpriteModelEvent(BusEventType<? extends BusEvent> busEventType, List<Sprite> sprites) {
 		super(busEventType);
 		this.sprites = sprites;
 	}
 	
-//	public Sprite getSprite() {
-//		return sprite;
-//	}
 	public List<Sprite> getSprites() {
 		return sprites;
 	}
