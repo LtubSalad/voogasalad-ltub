8a97b26593283256c752375dfbb39bd434db3499
JACOB CONROY <JRC63@duke.edu>
2017-04-24 00:06:48 -0400

    Started making period events for spawning of monsters

--- src/newengine/sprite/components/Spawner.java
+++ src/newengine/sprite/components/Spawner.java
@@ -1,21 +1,42 @@
 package newengine.sprite.components;
 
 import data.SpriteMakerModel;
+import gamedata.AuthDataTranslator;
+import newengine.events.SpriteModelEvent;
+import newengine.events.spawner.SpawnEvent;
+import newengine.events.spawner.SpawnPrefEvent;
+import newengine.events.timer.PeriodicEvent;
+import newengine.sprite.Sprite;
 import newengine.sprite.component.Component;
 import newengine.sprite.component.ComponentType;
 
 public class Spawner extends Component {
 	public static final ComponentType<SoundEffect> TYPE = new ComponentType<>(Spawner.class.getName());
-	private SpriteMakerModel mySpriteToSpawn;
+	private SpriteMakerModel mySpriteModel;
+	private Sprite mySprite;
+	private double secondsBetween;
+	private int totalNumber;
 	
 	public Spawner(SpriteMakerModel spriteToSpawn) {
-		mySpriteToSpawn = spriteToSpawn;
+		mySpriteModel = spriteToSpawn;
 	}
 
 	@Override
 	public void afterAdded() { 
+		AuthDataTranslator translator = new AuthDataTranslator(mySpriteModel);
+		mySprite = translator.getSprite();
 		
+		sprite.on(SpawnPrefEvent.SETPREFS, e -> {
+			this.totalNumber = e.getTotalNumber();
+			this.secondsBetween = e.getSecondBetween();
+			sprite.emit(new PeriodicEvent(totalNumber, secondsBetween, () -> sprite.emit(new SpawnEvent(SpawnEvent.SPAWN))));
+		});
+		sprite.on(SpawnEvent.SPAWN, (e) -> {
+			sprite.getComponent(GameBus.TYPE).get().getGameBus().emit(new SpriteModelEvent(SpriteModelEvent.ADD, mySprite.clone()));
+		});
 	}
+	
+	
 
 	@Override
 	public ComponentType<? extends Component> getType() {
