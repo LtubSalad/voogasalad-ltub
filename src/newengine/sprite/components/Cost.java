d51bcfe6b7423cdb598153f047630e6216faa9f8
ALISON <ah365@duke.edu>
2017-04-24 22:03:32 -0400

    making cost component
    Former-commit-id: da1d50cc53827fdddcfa8faa2175f82faf6cd925

---
+++ src/newengine/sprite/components/Cost.java
@@ -0,0 +1,29 @@
+package newengine.sprite.components;
+
+import newengine.sprite.component.Component;
+import newengine.sprite.component.ComponentType;
+
+public class Cost extends Component {
+	
+	public static final ComponentType<Cost> TYPE = new ComponentType<>(Cost.class.getName());
+	private int cost;
+	
+	public Cost(int cost){
+		this.cost = cost;
+	}
+	
+	public int getCost(){
+		return cost;
+	}
+
+	@Override
+	public ComponentType<? extends Component> getType() {
+		return TYPE;
+	}
+
+	@Override
+	public Component clone() {
+		return null;
+	}
+
+}
