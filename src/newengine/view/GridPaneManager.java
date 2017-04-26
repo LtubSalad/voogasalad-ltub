c0bdc77f3cd78085abb341510f0e02eb05a1da5d
Zhiyong Zhao <zhiyong.zhao@duke.edu>
2017-04-25 21:52:43 -0400

    more view construction

--- src/newengine/view/GridPaneManager.java
+++ src/newengine/view/GridPaneManager.java
@@ -3,8 +3,21 @@
  */
 package newengine.view;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
+import newengine.sprite.Sprite;
 
 /**
  * @author Zhiyong
@@ -14,6 +27,16 @@ import javafx.scene.layout.GridPane;
  *
  */
 public class GridPaneManager implements NodeManager<Node>{
+	
+	public static final int NUMER_OF_ROW_SKILL= 3;
+	public static final int NUMER_OF_COLUMN_SKILL= 3;
+	
+	public static final int NUMER_OF_ROW_SPRITE= 3;
+	public static final int NUMER_OF_COLUMN_SPRITE= 3;
+	
+	private Map<Pair<Integer, Integer>, Button> skillButtonMap = new HashMap<>();
+	private Map<Pair<Integer, Integer>, Button> spriteButtonMap = new HashMap<>();
+	private Map<Pair<Integer, Integer>, Button> selectedSpriteButtonMap = new HashMap<>();
 
 	@Override
 	public Node getNode() {
@@ -31,17 +54,61 @@ public class GridPaneManager implements NodeManager<Node>{
 
 	private Node getSkillsButton() {
 		// TODO Auto-generated method stub
-		return null;
+		GridPane buttonPane = new GridPane();
+		for(int i = 0; i < NUMER_OF_COLUMN_SKILL; i ++){
+			for(int j = 0; j < NUMER_OF_COLUMN_SKILL; j++){
+				Button button = new Button();
+				skillButtonMap.put(new Pair<Integer, Integer>(i, j ), button);
+			}
+		}
+		return buttonPane;
+	}
+	
+	
+	/**
+	 * @return the button of the skills
+	 * this information of the skill button will change according to the
+	 * corresponding skills of the  selected sprite 
+	 */
+	public ObservableMap<Pair<Integer, Integer>, Button> getSkillButtonMap(){
+		
+		return FXCollections.observableMap(skillButtonMap);
 	}
 
 	private Node getAllSpritesButton() {
-		// TODO Auto-generated method stub
-		return null;
+				GridPane buttonPane = new GridPane();
+				for(int i = 0; i < NUMER_OF_COLUMN_SPRITE; i ++){
+					for(int j = 0; j < NUMER_OF_COLUMN_SPRITE; j++){
+						Button button = new Button();
+						spriteButtonMap.put(new Pair<Integer, Integer>(i, j ), button);
+					}
+				}
+				return buttonPane;
+	}
+	
+	/**
+	 * @return the button of the skills
+	 * this information of the skill button will change according to the
+	 * corresponding skills of the  selected sprite 
+	 */
+	public ObservableMap<Pair<Integer, Integer>, Button> getSpritesButtonMap(){
+		
+		return FXCollections.observableMap(spriteButtonMap);
 	}
 
 	private Node getSelectedSpriteButton() {
 		// TODO Auto-generated method stub
-		return null;
+
+		Button button = new Button();
+		//button.backgroundProperty().bind();
+		return button;
+	}
+	
+	public ObservableList<Button> getSelectedSpriteButtonObseervable(){
+		List<Button> selectedSpriteButton = new ArrayList<Button>();
+		selectedSpriteButton.add((Button) getSelectedSpriteButton());
+		
+		return FXCollections.observableList(selectedSpriteButton);
 	}
 
 	private Node getSmallMap() {
