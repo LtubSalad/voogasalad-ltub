ccae1e9d449bcf092b8ff176c31fb2d1523abcea
Tahia Emran <tahia.emran@duke.edu>
2017-04-28 22:08:03 -0400

    made serilizable amount of data
    Former-commit-id: 5917d0e447bf17daf4cd9bc6a0726a377903bd12

---
+++ src/data/SerializableDeveloperData.java
@@ -0,0 +1,77 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import gamecreation.level.LevelData;
+import javafx.collections.ObservableList;
+
+public class SerializableDeveloperData {
+	private Map<String, String> gameData; 
+	private List<SpriteMakerModel> mySprites; 
+	private List<LevelData> myLevels; 
+	private SpritesForScreenUse screenSprites; 
+	private String gameName; 
+	private String gameIconFile; 
+	
+	DeveloperData dataToTranslate; // TODO: see if this serializes ok 
+	
+	private SerializableDeveloperData(DeveloperData data){
+		this.dataToTranslate = data; 
+		this.screenSprites = data.getScreenSprites();
+		this.gameName = data.getGameName();
+		this.gameIconFile = data.getGameIcon();
+		configData(); 
+		configSprites(); 
+		configLevels(); 
+		
+	}
+
+
+	private void configLevels() {
+		ObservableList<LevelData> levels = dataToTranslate.getLevelData();
+		myLevels = new ArrayList<LevelData>();
+		myLevels = levels.stream().collect(Collectors.toList());
+	}
+
+
+	private void configSprites() {
+		ObservableList<SpriteMakerModel> spriteData = dataToTranslate.getSprites();
+		mySprites = new ArrayList<SpriteMakerModel>();
+		mySprites = spriteData.stream().collect(Collectors.toList());
+	}
+
+	private void configData() {
+		gameData = new HashMap<String, String>();	
+		for (String dataName: dataToTranslate.getAllData().keySet()){
+			gameData.put(dataName, dataToTranslate.getAllData().get(dataName));
+		}
+		
+		//TODO: Delete when matt fixes 
+		gameData.put("GAME_NAME", this.gameName);
+		gameData.put("GAME_FILE", this.gameIconFile);
+	}
+	
+	public List<SpriteMakerModel> getSprites(){
+		return Collections.unmodifiableList(mySprites); 
+	}
+	
+	public List<LevelData> getLevels(){
+		return Collections.unmodifiableList(myLevels);
+	}
+	
+	public Map<String, String> getGameData(){
+		return Collections.unmodifiableMap(gameData);
+	}
+	
+	public SpritesForScreenUse getScreenSprites(){
+		return this.screenSprites;
+	}
+	
+
+	
+}
