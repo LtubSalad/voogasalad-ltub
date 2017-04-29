2feeb3af9e5660b5e4319ac5e8002c98c5fef758
JACOB CONROY <JRC63@duke.edu>
2017-04-29 18:18:42 -0400

    Pulled from master
    Former-commit-id: 543ed69c95afb9c7c1bccdb13dbfdf60a8c0fafc

---
+++ src/user/MessagingHistory.java
@@ -0,0 +1,59 @@
+package user;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javafx.util.Pair;
+
+public class MessagingHistory {
+	
+	private String myUserName; 
+	
+	private List<Message> myRecievedPosts; 
+	private	List<Message> mySentMessages;
+	private Map<String, List<Message>> myPrivateMessages;
+	
+	public MessagingHistory(String username){
+		this.myUserName = username; 
+		myRecievedPosts = new ArrayList<Message>();
+		mySentMessages = new ArrayList<Message>();
+		myPrivateMessages = new HashMap<String, List<Message>>();  // map of person being messaged by the user + the string of messages 
+	}
+	
+	public void recieve(String sender, String message){
+		Message newMessage = new Message(sender, message);
+		myRecievedPosts.add(newMessage);
+	}
+	
+	public void addSent(String recipient, String message){
+		System.out.println("message being added to send history for user " + myUserName);
+		mySentMessages.add(new Message(recipient, message));
+	}
+	
+	//TODO: fix this, time permitting 
+	public void sendPrivateMessage(String other, String message){
+		if (!myPrivateMessages.containsKey(other)){
+			myPrivateMessages.put(other, new ArrayList<Message>());
+		}
+		myPrivateMessages.get(other).add(new Message(other, message));
+	}
+	
+	public void recievePrivateMessage(String sender, String message){
+		if (!myPrivateMessages.containsKey(sender)){
+			myPrivateMessages.put(sender, new ArrayList<Message>());
+		}
+		myPrivateMessages.get(sender).add(new Message(sender, message));
+	}
+	
+	public Map<String, String> getDisplayableMessages(){
+		Map<String, String> dispMessages = new HashMap<String, String>(); 
+		myRecievedPosts.stream().forEach(message -> {
+			Pair<String,String> m = message.getComment();
+			dispMessages.put(m.getKey(), m.getValue());
+		});
+		return dispMessages; 
+	}
+
+}
--- src/user/UserTest.java
+++ src/user/UserTest.java
@@ -9,13 +9,31 @@ public class UserTest extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		User testuser = new User("me", "resources/green_house.jpg");
-		testuser.recordAuthored("Test Tower Defense", "yike.xml");
-		testuser.recordGamePlayed("Test Played", "yike.xml");
-		UserSocialPage page = new UserSocialPage(testuser);
-		Scene scene = new Scene(page);
+		//User testuser = new User("me", "resources/green_house.jpg");
+//		testuser.recordAuthored("Test Tower Defense", "yike.xml");
+//		testuser.recordGamePlayed("Test Played", "yike.xml");
+//		UserSocialPage page = new UserSocialPage(testuser);
+//		Scene scene = new Scene(page);
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
+		
+		UsersModel model = new UsersModel(); 
+		model.addUser("me", "1234");
+		model.addUser("not me", "4321");
+		User me = model.getUserByName("me");
+		User notMe = model.getUserByName("not me");
+		
+		notMe.sendMessage("me", "hi");
+		// is not me recieving
+		me.recordAuthored("test game auth", "yike.xml");
+		me.recordGamePlayed("test play", "yike.xml");
+		
+		UserSocialPage page = new UserSocialPage(me);
+		
+		Scene scene = new Scene(page, 600, 600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
 	}
 	
 	public static void main (String[] args){
