df655017ba1873ac5b5553d8619a670e3159059b
Tahia Emran <tahia.emran@duke.edu>
2017-04-29 14:11:44 -0400

    fixed compile error in the tester
    Former-commit-id: 6e30ba1c0033f453ac5c08e4f7f2d5eb69d0b744 [formerly b87f6af280bafaec3e6cab79713faace4c58abba]
    Former-commit-id: ceb5c4a07426c7832c785aad51e07e1fe419283c

--- src/user/UserTest.java
+++ src/user/UserTest.java
@@ -9,13 +9,13 @@ public class UserTest extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		User testuser = new User("me", "resources/green_house.jpg");
-		testuser.recordAuthored("Test Tower Defense", "yike.xml");
-		testuser.recordGamePlayed("Test Played", "yike.xml");
-		UserSocialPage page = new UserSocialPage(testuser);
-		Scene scene = new Scene(page);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		//User testuser = new User("me", "resources/green_house.jpg");
+//		testuser.recordAuthored("Test Tower Defense", "yike.xml");
+//		testuser.recordGamePlayed("Test Played", "yike.xml");
+//		UserSocialPage page = new UserSocialPage(testuser);
+//		Scene scene = new Scene(page);
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
 	}
 	
 	public static void main (String[] args){
