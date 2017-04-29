577723dc93e196c9974876bb407e76231affaf6b
JACOB CONROY <JRC63@duke.edu>
2017-04-29 18:18:42 -0400

    Pulled from master
    Former-commit-id: 2feeb3af9e5660b5e4319ac5e8002c98c5fef758 [formerly 543ed69c95afb9c7c1bccdb13dbfdf60a8c0fafc]
    Former-commit-id: 4023d0d5dc5531f03ad11ef08664d548cab6d424

--- src/data/SpriteMakerModel.java
+++ src/data/SpriteMakerModel.java
@@ -19,9 +19,11 @@ import newengine.sprite.component.ComponentType;
 
 public class SpriteMakerModel {
 	private Map<String, String> myCustomEventHandlers;
+
 	private Map<ComponentType<?>,Component> myComponents;
 	private Map<BusEvent, String> myScriptMap;
 	private String spriteName;
+	private String spriteDescription;
 	
 	// Jake and Tahia's DO NOT TOUCH
 	private Map<String, List<String>> componentsForTransfer; 
@@ -31,6 +33,8 @@ public class SpriteMakerModel {
 	List<Pair<BusEvent, BusEvent>> triggers; // event + Action for the trigger (custom event handling) 	
 	
 	public SpriteMakerModel() {
+		Map<ComponentType<?>, Component >componentMap=new HashMap<>();
+		Map<BusEvent, String> handlers=new HashMap<>();
 		myComponents=new HashMap<>();
 		myScriptMap=new HashMap<>();
 		actualComponents = new ArrayList<Component>(); 
@@ -53,6 +57,13 @@ public class SpriteMakerModel {
 		spriteName=name;
 	}
 
+	public String getDescription(){
+		return spriteDescription;
+	}
+	
+	public void setDescription(String description){
+		spriteDescription=description;
+	}
 	
 	public void addSkill(Skill skill){
 		skills.add(skill);
@@ -110,11 +121,11 @@ public class SpriteMakerModel {
 		return componentsForTransfer; 
 	}
 	
-	public SpriteMakerModel(SpriteMakerModel toCopy) {
-		this.myCustomEventHandlers = toCopy.myCustomEventHandlers;
-		this.myComponents = toCopy.myComponents;
-		this.myScriptMap = toCopy.myScriptMap;
-	}
+//	public SpriteMakerModel(SpriteMakerModel toCopy) {
+//		this.myCustomEventHandlers = toCopy.myCustomEventHandlers;
+//		this.myComponents = toCopy.myComponents;
+//		this.myScriptMap = toCopy.myScriptMap;
+//	}
 	
 	public SpriteMakerModel(String name){
 		this();
@@ -137,6 +148,7 @@ public class SpriteMakerModel {
 //			}
 //		});
 		actualComponents.add(comp);
+
 		myComponents.put(comp.getType(), comp);
 	}
 	
@@ -152,6 +164,18 @@ public class SpriteMakerModel {
 		return myComponents;
 	}
 	
+//	public void addScript(BusEvent event, String script) {
+//		myScriptMap.put(event, script);
+//	}
+//	
+//	public Map<BusEvent,String> getScriptMap() {
+//		return myScriptMap;
+//	}
+//	
+//	public ObservableMap<ComponentType<?>,Component> getComponents() {
+//		return myComponents;
+//	}
+//	
 	public Collection<BusEvent> getListenedEvents(){
 		List<BusEvent> dummyList=new ArrayList<>();
 		dummyList.add(new SpriteKilledEvent());
@@ -169,7 +193,7 @@ public class SpriteMakerModel {
 	}
 	
 	public Component getComponentByType(ComponentType<?> type) {
-		for (Component c : myComponents.values()) {
+		for (Component c : actualComponents) {
 			if (c.getType().equals(type)) {
 				return c;
 			}
@@ -187,15 +211,15 @@ public class SpriteMakerModel {
 	 * Call this AFTER unserialization to avoid the bus issues
 	 * @return
 	 */
-	public Sprite produceSprite(){
-		Sprite sprite=new Sprite();
-		myComponents.forEach((componentType, component)->{
-			sprite.addComponent(component);
-		});
-//		myScriptMap.forEach((event, script)->{
-//			sprite.produceHandler(event.getEventType(), script);
+//	public Sprite produceSprite(){
+//		Sprite sprite=new Sprite();
+//		myComponents.forEach((componentType, component)->{
+//			sprite.addComponent(component);
 //		});
-		return sprite;
-	}
+////		myScriptMap.forEach((event, script)->{
+////			sprite.produceHandler(event.getEventType(), script);
+////		});
+//		return sprite;
+//	}
 
 }
