158022f9b61962162334cda2f365d5583566452a
Keping <kw238@duke.edu>
2017-04-30 17:14:14 -0400

    game creator almost done
    Former-commit-id: 0b336070063c6bfa997961d1418eaaf7f1abf026 [formerly 93ab160a8644f647bcc78f73c53f03e0dcd12ff2]
    Former-commit-id: 4d47600026a4de9af9adf628d2e2dc4e1ed8ef1c

--- src/gamedata/GameCreator.java
+++ src/gamedata/GameCreator.java
@@ -1,7 +1,9 @@
 package gamedata;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.thoughtworks.xstream.XStream;
@@ -10,6 +12,7 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 import bus.EventBus;
 import data.DeveloperData;
 import data.SerializableDeveloperData;
+import data.SpriteMakerModel;
 import newengine.app.Game;
 import newengine.events.GameInitializationEvent;
 import newengine.events.SpriteModelEvent;
@@ -23,7 +26,14 @@ import newengine.managers.conditions.GoldMinimumCondition;
 import newengine.managers.conditions.NoLivesCondition;
 import newengine.model.PlayerStatsModel.WealthType;
 import newengine.player.Player;
+import newengine.skill.Skill;
+import newengine.skill.SkillType;
+import newengine.skill.skills.BuildSkill;
+import newengine.skill.skills.BuildSkillFactory;
 import newengine.sprite.Sprite;
+import newengine.sprite.SpriteID;
+import newengine.sprite.components.GameBus;
+import newengine.sprite.components.SkillSet;
 import player.helpers.GameLoadException;
 
 /**
@@ -39,6 +49,19 @@ public class GameCreator {
 		xstream = new XStream(new DomDriver());
 	}
 
+	private Sprite createTowerBuilder(Player player, List<SpriteMakerModel> availableTowers) {
+		Sprite towerBuilder = new Sprite(SpriteID.TOWER_BUILDER_ID);
+		towerBuilder.addComponent(new GameBus());
+		Map<SkillType<? extends Skill>, Skill> skillMap = new HashMap<>();
+		for (SpriteMakerModel availableTower : availableTowers) {
+			BuildSkill buildSkill = BuildSkillFactory.createBuildSkill(availableTower);
+			skillMap.put(buildSkill.getType(), buildSkill);
+		}
+		towerBuilder.addComponent(new SkillSet(skillMap));
+		// currently no image
+		return towerBuilder;
+	}
+	
 	public Game createGame(String gameFilePath) throws GameLoadException {
 		return createGame(new File(gameFilePath));
 	}
@@ -46,34 +69,29 @@ public class GameCreator {
 		try {
 			Game game = new Game();
 
-			// Read out the game
-			SerializableDeveloperData myData = (SerializableDeveloperData) xstream.fromXML(gameFile); // TODO
+			SerializableDeveloperData myData = (SerializableDeveloperData) xstream.fromXML(gameFile); 
 			// serializableDeveloperData?
 			
 			// sync the players authoring and game.
 			// player 1: the user, towers
 			// enemy: the monsters
-			Player player1 = myData.getUserPlayer();
+			Player userPlayer = myData.getUserPlayer();
 			
 			List<Sprite> sprites = myData.getLevels().get(0).getSpawners().stream().map(
 					(spriteMakerModel) -> (new AuthDataTranslator(spriteMakerModel)).getSprite()
 			).collect(Collectors.toList());
-
-			// TODO: tower builder
-//			 Sprite towerBuilder = new Sprite(new
-			// SpriteID("__TOWER_BUILDER"));
-			myData.getSprites();
+			sprites.add(createTowerBuilder(myData.getUserPlayer(), myData.getSprites()));
+			
 
 			EventBus bus = game.getBus();
-
 			bus.on(GameInitializationEvent.ANY, (e) -> {
 				bus.emit(new InitILevelsEvent(myData.getLevels()));
 				bus.emit(new SoundEvent(SoundEvent.BACKGROUND_MUSIC, "data/sounds/01-dark-covenant.mp3"));
 				bus.emit(new SpriteModelEvent(SpriteModelEvent.ADD, sprites));
 				
-				bus.emit(new MainPlayerEvent(player1));
-				bus.emit(new ChangeLivesEvent(ChangeLivesEvent.SET, player1, Integer.parseInt(myData.getGameData().get(DeveloperData.NUMBER_OF_LIVES))));
-				bus.emit(new ChangeWealthEvent(ChangeWealthEvent.CHANGE, player1, WealthType.GOLD, Integer.parseInt(myData.getGameData().get(DeveloperData.NUMBER_OF_STARTING_GOLD))));
+				bus.emit(new MainPlayerEvent(userPlayer));
+				bus.emit(new ChangeLivesEvent(ChangeLivesEvent.SET, userPlayer, Integer.parseInt(myData.getGameData().get(DeveloperData.NUMBER_OF_LIVES))));
+				bus.emit(new ChangeWealthEvent(ChangeWealthEvent.CHANGE, userPlayer, WealthType.GOLD, Integer.parseInt(myData.getGameData().get(DeveloperData.NUMBER_OF_STARTING_GOLD))));
 				
 				//TODO condition factory
 				bus.emit(new SetEndConditionEvent(SetEndConditionEvent.SETWIN, new GoldMinimumCondition(1000)));
